f6d4d058e624e26e3d5d2d34e172b96e9ed217d3
Boris Krasnov <crasnov.boris@yandex.ru>
2023-12-04 13:38:38 +0400

    Обработка ошибок

--- main.go
+++ main.go
@@ -1,25 +1,60 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
-func Home(w http.ResponseWriter,r *http.Request){
+ const portNumber = ":8080"
 
+// Home page handler
+func Home(w http.ResponseWriter,r *http.Request){
+	fmt.Fprintf(w,"This is a home Page")
 }
 
 
-func main() {
-	http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
-		
-		// Вывод hello world на экран
-		n,err := fmt.Fprintf(w,"Hello world")
-		if err != nil{
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d",n))
-	})
+// About is the about page handler
+func About(w http.ResponseWriter,r *http.Request){
+	sum := addValues(2,2)
+	_,_ = fmt.Fprintf(w,fmt.Sprintf("This is an about Page and 2 + 2 is %d",sum))
+}
 
-	http.ListenAndServe(":8080",nil)
+// addValues add two integers and returns sum
+func addValues(x,y int) (int) {
+	return x + y
+}
+
+func Divide(w http.ResponseWriter,r *http.Request){
+	
+	f,err := divideValues(100.0,0.0)
+	if err !=nil{
+		fmt.Fprintf(w,"Cannot divide by 0")
+		// return прерывает работу данной функции
+		return
+	}
+
+	fmt.Fprintf(w,fmt.Sprintf("%f divided by %f is %f",100.0,0.0,f))
+}
+
+func divideValues(x,y float32) (float32,error){
+	
+	if y <= 0{
+		err := errors.New("cannot divide by zero")
+		return 0,err
+	}
+	result := x / y 
+	return result,nil
+
+}
+
+// main is the main app function
+func main() {
+	http.HandleFunc("/",Home)
+	http.HandleFunc("/about",About)
+	http.HandleFunc("/divide",Divide)
+
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s",portNumber))
+	http.ListenAndServe(portNumber,nil)
 }
